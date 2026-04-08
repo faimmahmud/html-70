81ffb3e9d979171cb5f209887dae608374a25b5b
faimmahmud <faimmahmud25@gmail.com>
2026-04-08 11:43:59 +0600

    ds

--- php/any/student_info.php
+++ php/any/student_info.php
@@ -1,22 +1,18 @@
 <?php
-// car.php ফাইলটি include করা (class ব্যবহার করার জন্য)
+
 require_once('car.php');
 
-// form submit হয়েছে কিনা check করা
 if (isset($_POST['submit'])) {
 
-    // form থেকে data নেওয়া
-    $id = $_POST['id'];        // ID input থেকে value নেওয়া
-    $name = $_POST['name'];    // Name input থেকে value নেওয়া
-    $email = $_POST['email'];  // Email input থেকে value নেওয়া
 
-    // Car class এর object তৈরি করা
+    $id = $_POST['id'];      
+    $name = $_POST['name'];    
+    $email = $_POST['email'];
+
     $car = new Car($id, $name, $email);
 
-    // data file-এ save করা
     $car->saveData();
 
-    // success message দেখানো
     echo "Data Saved Successfully<br><br>";
 }
 ?>
@@ -24,33 +20,29 @@ if (isset($_POST['submit'])) {
 <!DOCTYPE html>
 <html>
 <head>
-    <title>Student Info</title> <!-- পেজের title -->
+    <title>Student Info</title>
 </head>
 <body>
 
-<!-- form তৈরি করা -->
+
 <form method="post">
     
     ID: <br>
-    <!-- number input (ID নেওয়ার জন্য) -->
     <input type="number" name="id" required><br><br>
 
     Name: <br>
-    <!-- text input (Name নেওয়ার জন্য) -->
     <input type="text" name="name" required><br><br>
 
     Email: <br>
-    <!-- email input (Email নেওয়ার জন্য) -->
     <input type="email" name="email" required><br><br>
 
-    <!-- submit button -->
     <input type="submit" name="submit" value="Submit">
 </form>
 
 <hr>
 
 <?php
-// static method call করে সব data দেখানো
+
 Car::showData();
 ?>
 
